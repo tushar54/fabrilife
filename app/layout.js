58791592de440fa5052c593f56d3d0edba9429be
tushar54 <tusharahmed153216@gmail.com>
2025-08-31 02:29:43 +0600

    Actually i want to learn next.js through the next.js project and documentation

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Allcomponent/Navbar";
+import Footer from "./Allcomponent/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
         <Navbar></Navbar>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
