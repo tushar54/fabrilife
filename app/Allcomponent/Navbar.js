31aa7b52f86ff5a2eee559c659300d2f7d23d83a
tushar54 <tusharahmed153216@gmail.com>
2025-08-31 23:19:23 +0600

    i have included Home,contact,and aboutus route inside the project

--- app/Allcomponent/Navbar.js
+++ app/Allcomponent/Navbar.js
@@ -3,6 +3,11 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
+  const a=<> <li><Link href={'/'}>Home</Link></li>
+        <li>
+          <Link href={'/contactus'}>Contact Us</Link>
+        </li>
+        <li><Link href={'/aboutus'}>About us</Link></li></>
   return (
    <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
@@ -13,32 +18,14 @@ export default function Navbar() {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+       {a}
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {a}
     </ul>
   </div>
   <div className="navbar-end">
